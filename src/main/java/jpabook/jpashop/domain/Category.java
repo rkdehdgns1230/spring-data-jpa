4012d950ef61420b96756f042b531797062613d4
5004053 <skp.5004053@sk.com>
2023-05-26 14:53:39 +0900

    feature - 연관관계 편의 메서드 개발, cascade option setting

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -8,6 +8,8 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 @Getter @Setter
 public class Category {
@@ -24,10 +26,15 @@ public class Category {
     )
     private List<Item> items = new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
+
+    public void setChildCategory(Category category){
+        this.child.add(category);
+        category.setParent(this);
+    }
 }
